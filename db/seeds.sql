7cb6a40b0eba8a7b02d0e84940ab9079e6853191
DMFletcher <thanatos.02@gmail.com>
2018-06-16 02:35:29 -0500

    npm init

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bobbi');
+INSERT INTO burgers (burger_name) VALUES ('Meeses');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden', false);
+INSERT INTO burgers (burger_name) VALUES ('Owens');
